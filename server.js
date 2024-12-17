8dfefacdc0f7edac0b4ec7542369a75c113aa7b0
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-17 21:04:18 +0300

    setheader created

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const PORT = 8000;
 
 const server = http.createServer((req, res) => {
     // res.write('Hello World!');
-    res.end('Hello World!');
+    // res.end('Hello World!');
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 404;
+
+    res.end('<h1>Hello World!</h1>');
 });
 
 server.listen(PORT, () => {
