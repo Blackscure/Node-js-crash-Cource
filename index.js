3f80e6f7a155f34e6059a1e7f6bda831cc098979
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-17 20:13:25 +0300

    es added

---
+++ index.js
@@ -0,0 +1,6 @@
+const { generateRandomNumber, celciusToFahreheight} = require('./utils');
+
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`celcius to fahrenhiet ${celciusToFahreheight(0)}`);
