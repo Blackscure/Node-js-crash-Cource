f9b950225272ae99f1ef149ca630246c06e8b6cc
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-17 20:56:14 +0300

    servercreated using create serever method

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const { generateRandomNumber, celciusToFahreheight} = require('./utils');
+import getPosts, { getPostsLenght }  from "./postController.js";
+
+console.log(getPosts());
+
+console.log(`Posts Lenght: ${getPostsLenght()}`);
+
+// const { generateRandomNumber, celciusToFahreheight} = require('./utils');
 
 
-console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Random Number: ${generateRandomNumber()}`);
 
-console.log(`celcius to fahrenhiet ${celciusToFahreheight(0)}`);
+// console.log(`celcius to fahrenhiet ${celciusToFahreheight(0)}`);
