2fa5be1445f6289a275016dc6ea4266a2206009a
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-17 21:09:26 +0300

    json return

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const PORT = 8000;
 const server = http.createServer((req, res) => {
     // res.write('Hello World!');
     // res.end('Hello World!');
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 404;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
 
-    res.end('<h1>Hello World!</h1>');
+    res.writeHead(500, { 'Content-Type': 'application/json'})
+
+    res.end(JSON.stringify({ message: 'Server Error'}));
 });
 
 server.listen(PORT, () => {
